f1882fbe28015c15996d1fb62526bb7a7fabc1b0
arnaud-ma <arnaudma.code@gmail.com>
2026-04-10 13:59:41 +0200

    JLD2 saving: do not serialize the entire zipfile

---
+++ ext/JLD2Ext.jl
@@ -0,0 +1,21 @@
+module JLD2Ext
+
+import JLD2
+using ECAD: VariableData, Variable
+
+struct VariableDataSerial
+    variable::Variable
+    filepath::String
+end
+
+JLD2.writeas(::Type{<:VariableData}) = VariableDataSerial
+
+function JLD2.wconvert(::Type{VariableDataSerial}, v::VariableData)
+    VariableDataSerial(v.variable, v.filepath)
+end
+
+function JLD2.rconvert(::Type{<:VariableData}, v::VariableDataSerial)
+    VariableData(v.variable, v.filepath; memory_map=true)
+end
+
+end # module JLD2Ext
